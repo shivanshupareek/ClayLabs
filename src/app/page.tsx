ac972023ebc2b833adc87b4f50d8193f5391fce6
shivanshupareek <shivanshupareek.ps@gmail.com>
2026-05-19 14:03:14 +0530

    chore(layout): comment out VisualBreak and add top padding to footer
    - Comment out VisualBreak (footer image) for layout review
    - Add padding-top to footer: 64px mobile, 96px desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <KidsClassesSection />
         <KilnFiringSection />
       </main>
-      <VisualBreak />
+      {/* <VisualBreak /> */}
       <Footer />
     </>
   );
